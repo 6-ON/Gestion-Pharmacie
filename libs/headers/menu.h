ce3dbd73b9d460b7b434f3a3f7cb21db412eec46
6-on <=aminhatim6@gmail.com>
2022-09-20 12:39:49 -0400

    finish add product view

--- libs/headers/menu.h
+++ libs/headers/menu.h
@@ -1,4 +1,7 @@
 #ifndef MENU_H
 #define MENU_H
+#include "../headers/product_list.h"
+
+
 void Menu();
 #endif
--- libs/sources/menu.c
+++ libs/sources/menu.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "../headers/menutils.h"
+#include "../headers/product_list.h"
+#include "../headers/product.h"
+#include "../headers/controller.h"
 #include <stdbool.h>
 
+extern ProductList pl;
+
+// input functions
 void getInt(int *val)
 {
     while (true)
@@ -33,77 +40,127 @@ void getDouble(double *val)
         break;
     }
 }
+// Prints Menu
 void printMenu()
 {
     printf(
-        AJOUTER"\n"LISTER_PRD"\n"
-        ACHETER"\n"SEARCH"\n"
-        ESTOCK"\n"SUPP_PROD"\n"
-        ST_VNT"\n"QUITTER"\n>> "
-    );
+        AJOUTER "\n" LISTER_PRD "\n" ACHETER "\n" SEARCH "\n" ESTOCK "\n" SUPP_PROD "\n" ST_VNT "\n" QUITTER "\n>> ");
 }
 
-void showAddProductView(){
-    
-}
-void showListProductsView(){
+// done
+void showAddProductView()
+{
+    system(CLEAR);
+    char init_name[255];
+    int init_quantity;
+    double init_price;
+    int count;
+    printf(PROMPT_NPRODUCTS);
+    getInt(&count);
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d\n",i);
+        printf(PROMPT_PRODUCT_NAME);
 
-}
-void showPurchaseView(){
+        fgets(init_name, sizeof(init_name), stdin);
+        init_name[strcspn(init_name, "\n")] = 0;
+    renter_price:
+        printf(PROMPT_PRODUCT_PRICE);
+        getDouble(&init_price);
+        if (init_price < 0)
+        {
+            puts(ERR_NEGATIVE_PRICE);
+            goto renter_price;
+        }
 
-}
-void showSearchView(){
+    renter_quantite:
+        printf(PROMPT_QTTY);
+        getInt(&init_quantity);
+        if (init_quantity < 0)
+        {
+            puts(ERR_NEGATIVE_QTTY);
+            goto renter_quantite;
+        }
+        addProduct(&pl, newProduct(init_name, init_price, (unsigned int)init_quantity));
+    }
 
+    writeData(&pl);
+    puts(PRODUCT_ADDED);
 }
-void showStockStateView(){
+void showListProductsView()
+{
+    system(CLEAR);
+    printProductList(pl);
+    puts("<--0");
+    puts(">>");
+    int input;
+    do
+    {
+        getInt(&input);
+    } while (input != 0);
+}
+void showPurchaseView()
+{
+    system(CLEAR);
+}
+void showSearchView()
+{
+    system(CLEAR);
+    printf("V4");
+}
+void showStockStateView()
+{
+    system(CLEAR);
+    printf("V5");
+}
+void showDeleteProductView()
+{
+    system(CLEAR);
+    printf("V6");
+}
+void showStatisticsView()
+{
+    system(CLEAR);
+    printf("V7");
+}
 
-}
-void showDeleteProductView(){
-
-}
-void showStatisticsView(){
-
-}
-void Menu(){
+void Menu()
+{
     system(CLEAR);
     printMenu();
     int choice;
     getInt(&choice);
     switch (choice)
     {
-    case CH_AJOUTER :
+    case CH_AJOUTER:
         showAddProductView();
         break;
     case CH_LISTER_PRD:
         showListProductsView();
         break;
-    case CH_ACHETER :
+    case CH_ACHETER:
         showPurchaseView();
         break;
-    case CH_SEARCH :
+    case CH_SEARCH:
         showSearchView();
         break;
-    case CH_ESTOCK :
+    case CH_ESTOCK:
         showStockStateView();
         break;
-    case CH_SUPP_PROD :
+    case CH_SUPP_PROD:
         showDeleteProductView();
         break;
-    case CH_ST_VNT :
+    case CH_ST_VNT:
         showStatisticsView();
         break;
-    case CH_QUITTER :
+    case CH_QUITTER:
         puts(QUIT_MESSAGE);
         exit(EXIT_SUCCESS);
         break;
-    
+
     default:
-    puts(ERR_WRONG_INPUT);
+        puts(ERR_WRONG_INPUT);
         break;
     }
-
-
-
-
+    puts("retour a menu ...");
 }
-
