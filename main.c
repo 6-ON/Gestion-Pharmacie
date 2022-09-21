71d9417ab79fc8c3f4dd248903d3cac2174759c2
6-on <=aminhatim6@gmail.com>
2022-09-21 12:36:56 -0400

    update main

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include "libs/headers/product_list.h"
 #include "libs/headers/menu.h"
 #include "libs/headers/controller.h"
 #define REFRESH_DELAY 2
 #define PROD_CODE_START 1000000000
 
 ProductList pl;
+PurchaseList purl;
+unsigned int currentprodCode;
 
 int main(int argc, char const *argv[])
 {
 
-    pl = getData();
-
+    pl = getProductsData();
+    //get records here
+    purl = getRecordsData();
     currentprodCode = (pl.length > 0) ? pl.elements[pl.length - 1].code + 1 : PROD_CODE_START;
 
-    // currentprodCode = PROD_CODE_START; // product codes starts from here
     // ProductList pl= newProductList();
     // Product p1= newProduct("morphine",23,7);
     // Product p2= newProduct("Pomada safra",306.45,7);
